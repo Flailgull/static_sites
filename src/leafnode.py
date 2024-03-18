fe62bdc696d9cc566f31a03a4f80299eed20a454
Flailgull <flailgull@gmail.com>
2024-03-18 04:00:04 +0100

    ch 2, ex 4: LeafNode - Der ValueError soll im to_html() und nicht im Konstruktor kommen

--- src/leafnode.py
+++ src/leafnode.py
@@ -3,15 +3,14 @@ from htmlnode import HTMLNode
 class LeafNode(HTMLNode):
 
     def __init__(self, tag = None, value = None, props = None):
-        if value is None:
-            raise ValueError
-
         super().__init__(tag, value, None, props)
 
     def __repr__(self) -> str:
         return f"LeafNode(tag - {self.tag}, value - {self.value}, props - {self.props})"
 
     def to_html(self) -> str:
+        if value is None:
+            raise ValueError
         if self.tag is None:
             return self.value
         
