6a7dfe3d109882aeafba8a79b32f648f4c1e907b
Flailgull <flailgull@gmail.com>
2024-03-18 02:21:14 +0100

    ch2, ex 3: HTMLNode - Es wird ein HTMLNode hinzugefügt und tests dafür geschrieben

---
+++ src/htmlnode.py
@@ -0,0 +1,22 @@
+class HTMLNode:
+
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self) -> str:
+        if self.props is None:
+            return ""
+        
+        result = ""
+        for key in self.props:
+            result += " " + key + "=\"" + self.props[key] + "\""
+        return result[1:]
+    
+    def __repr__(self) -> str:
+        return f"HTMLNode: tag - {self.tag}, value - {self.value}, children - {self.children}, props - {self.props}"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,30 @@
+import unittest
+
+from htmlnode import (
+    HTMLNode
+)
+
+class TestTextNode(unittest.TestCase):
+    def test_repr(self):
+        node = HTMLNode("tag", "value", "children", "props")
+        expected_text = "HTMLNode: tag - tag, value - value, children - children, props - props"
+        node_text = node.__repr__()
+        self.assertEqual(node_text, expected_text)
+
+    def test_props_to_html(self):
+        node = HTMLNode("tag", "value", "children", {"dummy": "dummy_value", "dummy2": "dummy2_value"})
+        expected_text = "dummy=\"dummy_value\" dummy2=\"dummy2_value\""
+        self.assertEqual(node.props_to_html(), expected_text)
+
+    def test_to_html_not_implemented(self):
+        node =  HTMLNode("tag", "value", "children", {"dummy": "dummy_value", "dummy2": "dummy2_value"})
+        with self.assertRaises(NotImplementedError):
+            node.to_html()
+
+    def test_props_to_html_none(self):
+        node = HTMLNode("tag", "value", "children", None)
+        expected_text = ""
+        self.assertEqual(node.props_to_html(), expected_text)
+
+if __name__ == "__main__":
+    unittest.main()
