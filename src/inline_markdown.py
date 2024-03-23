cd4578b9fe6fcec31b936fcc7740fd5f482e5d71
Flailgull <flailgull@gmail.com>
2024-03-23 03:33:06 +0100

    ch 3, ex 3: Tests aus der Solution kopiert und nochmal Code angepasst - scheint ich hatte nicht alles abgedeckt

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -6,19 +6,21 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for node in old_nodes:
         if node.text_type != text_type_text:
             new_nodes.append(node)
-        else:
-            substrings = node.text.split(delimiter)
-            if len(substrings) % 2 == 0:
-                raise ValueError("Closing delimiter not found")
+            continue
+        substrings = node.text.split(delimiter)
+        if len(substrings) % 2 == 0:
+            raise ValueError("Closing delimiter not found")
+        
+        is_text = substrings[0] != ""
+        for substring in substrings:
+            if is_text:
+                if substring != "":
+                    new_nodes.append(TextNode(substring, text_type_text))
+                is_text = False
             else:
+                if substring != "":
+                    new_nodes.append(TextNode(substring, text_type))
                 is_text = True
-                for substring in substrings:
-                    if is_text:
-                        new_nodes.append(TextNode(substring, text_type_text))
-                        is_text = False
-                    else:
-                        new_nodes.append(TextNode(substring, text_type))
-                        is_text = True
         
     return new_nodes
 
@@ -34,7 +36,8 @@ def split_nodes_image(old_nodes):
         post_image = old_node.text
         for image_tuple in image_tuples:
             pre_image, post_image = post_image.split(f"![{image_tuple[0]}]({image_tuple[1]})", 1)
-            new_nodes.append(TextNode(pre_image, text_type_text))
+            if pre_image != "":
+                new_nodes.append(TextNode(pre_image, text_type_text))
             new_nodes.append(TextNode(image_tuple[0], text_type_image, image_tuple[1]))
         if post_image != "":
             new_nodes.append(TextNode(post_image, text_type_text))
@@ -54,7 +57,8 @@ def split_nodes_link(old_nodes):
         post_link = old_node.text
         for link_tuple in link_tuples:
             pre_link, post_link = post_link.split(f"[{link_tuple[0]}]({link_tuple[1]})", 1)
-            new_nodes.append(TextNode(pre_link, text_type_text))
+            if pre_link != "":
+                new_nodes.append(TextNode(pre_link, text_type_text))
             new_nodes.append(TextNode(link_tuple[0], text_type_link, link_tuple[1]))
         if post_link != "":
             new_nodes.append(TextNode(post_link, text_type_text))
