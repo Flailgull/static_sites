df6549697a4fa2d6a62da31396aea48401dc1320
Flailgull <flailgull@gmail.com>
2024-03-23 04:46:42 +0100

    ch 4, ex 1: Splitting Blocks. - This is mostly a copy pasta from the solution, since i fucked up and forgot the '.py' in the tests, thereby making my code effectively untested. Didn't find that until after i gave up. Did confirm my code ran by running it through python itself (good to know that just assumes it's gonna be handed python-code)

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ from textnode import *
 from htmlnode import HTMLNode
 from leafnode import LeafNode
 from parentnode import ParentNode
+from block_markdown import *
 
 
 def main():
     test = TextNode("text", "text_type", "url")
     print(test)
+
 
 main()
