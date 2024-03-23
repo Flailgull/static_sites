9215aa0dc4007430a2d0e3826b934defac414c8c
Flailgull <flailgull@gmail.com>
2024-03-23 03:44:53 +0100

    ch 3, ex 4: Function text_to_textnodes, that puts together the last few chapters and some new test for it (probly not enough, gonna copy the official tests again later)

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -89,6 +89,24 @@ class TestInlineMarkdown(unittest.TestCase):
         ]
         self.assertEqual(new_nodes, excpected_nodes)
 
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+        expected_nodes = [
+            TextNode("This is ", text_type_text),
+            TextNode("text", text_type_bold),
+            TextNode(" with an ", text_type_text),
+            TextNode("italic", text_type_italic),
+            TextNode(" word and a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" and an ", text_type_text),
+            TextNode("image", text_type_image, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and a ", text_type_text),
+            TextNode("link", text_type_link, "https://boot.dev"),
+        ]
+        actual_nodes = text_to_textnodes(text)
+        self.assertEqual(expected_nodes, actual_nodes)
+
+
     #mehr tests - die hier aus der solution kopier, um zu sehen, ob ich sachen abgedeckt hab
     def test_delim_bold(self):
         node = TextNode("This is text with a **bolded** word", text_type_text)
